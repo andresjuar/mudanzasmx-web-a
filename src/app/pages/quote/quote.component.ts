1c212cba9f9bfe9c4e2610a4b289563a1982d2fa
andresjuar <andresjuarez14634@gmail.com>
2025-02-21 14:22:03 -0600

    Cambios Css y Cotizador

--- src/app/pages/quote/quote.component.ts
+++ src/app/pages/quote/quote.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 
-import { FooterComponent } from "../../shared/footer/footer.component";
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @Component({
   selector: 'app-quote',
-  imports: [ FooterComponent, FormsModule, CommonModule, QuoteFormComponent],
+  imports: [  FormsModule, CommonModule, QuoteFormComponent],
   templateUrl: './quote.component.html',
   styleUrl: './quote.component.css'
 })
