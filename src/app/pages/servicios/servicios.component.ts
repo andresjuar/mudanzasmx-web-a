dc9b8772de05a13d002e17a10cfff9e419a33132
andresjuar <andresjuarez14634@gmail.com>
2025-03-05 08:00:12 -0600

    1

--- src/app/pages/servicios/servicios.component.ts
+++ src/app/pages/servicios/servicios.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../shared/header/header.component";
 import { FooterComponent } from "../../shared/footer/footer.component";
-import { ServComponent } from "./serv/serv.component";
+import { PlanesComponent } from "../../shared/planes/planes.component";
 
 @Component({
   selector: 'app-servicios',
-  imports: [HeaderComponent, FooterComponent, ServComponent],
+  imports: [HeaderComponent, FooterComponent, PlanesComponent],
   templateUrl: './servicios.component.html',
   styleUrl: './servicios.component.css'
 })
