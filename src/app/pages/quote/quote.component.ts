5b763948bff3b4fbe30a606ee32fff2b840a3333
andresjuar <andresjuarez14634@gmail.com>
2025-03-08 18:46:59 -0600

    css arreglado formulario

--- src/app/pages/quote/quote.component.ts
+++ src/app/pages/quote/quote.component.ts
@@ -4,11 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { FooterComponent } from "../../shared/footer/footer.component";
-import { HeaderComponent } from "../../shared/header/header.component";
 
 @Component({
   selector: 'app-quote',
-  imports: [FormsModule, CommonModule, QuoteFormComponent, FooterComponent, HeaderComponent],
+  imports: [FormsModule, CommonModule, QuoteFormComponent, FooterComponent, 
+    
+  ],
   templateUrl: './quote.component.html',
   styleUrl: './quote.component.css'
 })
